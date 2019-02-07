9b6a5eeff51299740a762dad45ae2a612e05781c
samuelhegner <C16439262@mydit.ie>
2019-02-07 15:33:37 +0000

    Enemies (25%)

---
+++ PocketStrategy_2D/Assets/Enemy_Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+
+    int current = 1;
+
+    public float speed;
+
+    GameObject[] myPath;
+
+
+    void Awake()
+    {
+        myPath = GameObject.FindObjectOfType<Path_Creation>().path.ToArray();
+        transform.position = myPath[0].transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, myPath[current].transform.position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, myPath[current].transform.position) < 0.1f) {
+
+            if (current == myPath.Length - 1)
+            {
+                ReachedEnd();
+                gameObject.SetActive(false);
+            }
+            else {
+                current++;
+            }
+        }
+    }
+
+    void ReachedEnd() {
+        //whatever happens when the enemy hits the last path point
+
+        GameObject.FindObjectOfType<Wave_Manager>().activeEnemies.Remove(this.gameObject);
+        current = 1;
+    }
+}
--- PocketStrategy_2D/Assets/Wave_Manager.cs
+++ PocketStrategy_2D/Assets/Wave_Manager.cs
@@ -8,12 +8,36 @@ public class Wave_Manager : MonoBehaviour
 
     public float timeBetweenSpawn;
 
-    
+    public float numberOfEnemies;
+
+    public int bombEveryNormals;
+
+    public List<GameObject> activeEnemies;
+
+    Object_Pool pool;
+
+    Vector3 firstPoint;
+
+
+
+    void Start()
+    {
+        firstPoint = GameObject.FindObjectOfType<Path_Creation>().path[0].transform.position;
+        pool = Object_Pool.Instance;
+        StartCoroutine(SpawnWave());
+    }
+
 
     IEnumerator SpawnWave() {
-        while (activeWave) {
+        while (true) {
+            if (activeWave) {
+                
+                GameObject newEnemy = pool.SpawnFromPool("Normal", firstPoint, Quaternion.identity);
+                activeEnemies.Add(newEnemy);
+            }
+            
 
-
+            
             yield return new WaitForSeconds(timeBetweenSpawn);
         }
     }
