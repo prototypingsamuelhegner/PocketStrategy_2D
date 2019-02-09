a7269d98ca86d1e9a5eed294fc4b153cdd254bda
samuelhegner <C16439262@mydit.ie>
2019-02-09 12:20:19 +0000

    Health stuff start

---
+++ PocketStrategy_2D/Assets/Health_Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_Script : MonoBehaviour
+{
+    public int startHealth;
+
+    int health;
+
+    public bool destroyOnDeath, animationOnDeath, explodeOnDeath;
+
+    Animator anim;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Death() {
+        if (explodeOnDeath) {
+            
+        }
+        if (animationOnDeath) {
+
+        }
+        if (destroyOnDeath) {
+
+        }
+        yield return null;
+    }
+
+}
