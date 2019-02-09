f0236f34b88e83ce5542a5eb8a099d321d4b995b
samuelhegner <C16439262@mydit.ie>
2019-02-09 14:14:45 +0000

    Health System Basics

--- PocketStrategy_2D/Assets/Health_Script.cs
+++ PocketStrategy_2D/Assets/Health_Script.cs
@@ -6,6 +6,8 @@ public class Health_Script : MonoBehaviour
 {
     public int startHealth;
 
+    bool deathCalled;
+
     int health;
 
     public bool destroyOnDeath, animationOnDeath, explodeOnDeath;
@@ -15,25 +17,45 @@ public class Health_Script : MonoBehaviour
 
     void Start()
     {
-        
+        deathCalled = false;
     }
 
     void Update()
     {
-        
+        if (health <= 0 && !deathCalled) {
+            StartCoroutine(Death());
+            deathCalled = true;
+        }
     }
 
     IEnumerator Death() {
         if (explodeOnDeath) {
-            
+            gameObject.SendMessage("Explode");
         }
         if (animationOnDeath) {
-
+            anim.SetTrigger("Death");
         }
         if (destroyOnDeath) {
-
+            if (animationOnDeath)
+            {
+                yield return new WaitForEndOfFrame();
+                float clipLength = anim.GetCurrentAnimatorStateInfo(0).length;
+                yield return new WaitForSeconds(clipLength);
+                Destroy(gameObject);
+            }
+            else {
+                Destroy(gameObject);
+            }
         }
         yield return null;
+    }
+
+    public void RemoveHealth(int healthToLose) {
+        health -= healthToLose;
+    }
+
+    public void AddHealth(int healthToAdd) {
+        health += healthToAdd;
     }
 
 }
