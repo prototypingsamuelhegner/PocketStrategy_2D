5297b3925703d5bb1ba52f88db4cf1b1b863947e
samuelhegner <C16439262@mydit.ie>
2019-02-12 21:09:34 +0000

    Cracked Tile Script

---
+++ PocketStrategy_2D/Assets/CrackTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackTile : MonoBehaviour
+{
+    SpriteRenderer rend;
+
+    public Sprite[] possibleCracks;
+
+
+    float spriteAlpha;
+
+    void Update()
+    {
+        if (spriteAlpha > 0)
+        {
+            spriteAlpha *= (1.0f - (0.01f * Time.deltaTime));
+        }
+        else if (spriteAlpha < 0) {
+            spriteAlpha = 0;
+        }
+    }
+
+    public void StartFade() {
+        int ran = Random.Range(0, possibleCracks.Length);
+
+        rend.sprite = possibleCracks[ran];
+        spriteAlpha = 1;
+    }
+}
